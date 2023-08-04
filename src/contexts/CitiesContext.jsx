ef52dd004ffd4b3c708a29b2ba3a325dd502c0bf
Nikesh Adhikari <110375186+ultimatum254@users.noreply.github.com>
2023-08-04 12:04:27 +0545

    delete implemented

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -58,6 +58,21 @@ function CitiesProvider({ children }) {
     }
   }
 
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: 'DELETE',
+      });
+
+      setCities(cities => cities.filter(city => city.id !== id))
+    } catch (err) {
+      alert("Err on deleting city");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
@@ -66,6 +81,7 @@ function CitiesProvider({ children }) {
         currentCity,
         getCity,
         createCity,
+        deleteCity,
       }}
     >
       {children}
